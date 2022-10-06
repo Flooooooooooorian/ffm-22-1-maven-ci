e730895f17cbcf9500a4ab76ad8edd0b3c2bca7c
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2022-10-06 10:15:46 +0200

    Add Basic Test

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,22 @@
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+
+        //GIVEN
+        int a = 3;
+        int b = 6;
+
+        //WHEN
+        int actual = Calculator.add(a, b);
+
+        //THEN
+        int expected = 9;
+        assertEquals(expected, actual);
+    }
+}
